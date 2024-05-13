787fc913d8ccbe067cde85d5cba885de988cd897
Franz Genegobis <genegobis.franz@gmail.com>
2024-05-14 03:17:59 +0800

    Changed generics to viewset.

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.template.defaulttags import url
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
 from .View import *
+
+router = SimpleRouter()
+router.register('user', UserView)
 
 urlpatterns = [
     path('ai/', GPTApiView.as_view()),
-    path('user/', UserView.as_view())
+    path('', include(router.urls))  # Correct usage of include
 ]
 
