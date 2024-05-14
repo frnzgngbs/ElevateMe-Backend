309f137a028e7d86cd9045f9b4659a76d66772eb
Franz Genegobis <genegobis.franz@gmail.com>
2024-05-14 21:04:34 +0800

    Created Model package in which models are found.

--- api/Model/ProblemStatement.py
+++ api/Model/ProblemStatement.py
@@ -1,29 +1,6 @@
 from django.db import models
-
 from django.contrib.auth.models import User
-
-class VennDiagram(models.Model):
-    field1 = models.TextField()
-    field2 = models.TextField()
-    field3 = models.TextField()
-    filter = models.TextField(null=True)
-    user_fk = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    class Meta:
-        abstract = True
-
-    def __str__(self):
-        return f"{self.field1} | {self.field2} | {self.field3} | {self.user_fk}"
-
-class TwoVennDiagram(VennDiagram):
-    pass
-    def __str__(self):
-        return f"{self.field1} | {self.field2}"
-
-class ThreeVennDiagram(VennDiagram):
-    def __str__(self):
-        return f"{self.field1} | {self.field2} | {self.field3}"
-
+from .VennDiagram import TwoVennDiagram, ThreeVennDiagram
 class ProblemStatement(models.Model):
     statement = models.TextField()
     user_fk = models.ForeignKey(User, on_delete=models.CASCADE)
