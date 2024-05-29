05817c749140a6ae2fd0b017223c8cb76860b0f2
Franz Genegobis <genegobis.franz@gmail.com>
2024-05-29 18:07:25 +0800

    Minor changes on create.

--- api/View/ProblemStatementView.py
+++ api/View/ProblemStatementView.py
@@ -20,31 +20,17 @@ class TwoVennProblemStatementView(mixins.ListModelMixin,
     queryset = TwoVennProblemStatementModel.objects.all()
     serializer_class = TwoVennProblemStatementSerializer
 
-    def create(self, request, *args, **kwargs):
-        statements = request.data.pop("statement")
-
-        results = []
-        errors = []
-
-        for statement in statements:
-            item_data = request.data.copy()
-            item_data['statement'] = statement
-            item_data['user'] = request.user.pk
-
-            serializer = self.get_serializer(data=item_data)
-            if serializer.is_valid():
-                serializer.save(user=request.user)
-                results.append(serializer.data)
-            else:
-                errors.append(serializer.errors)
-
-        if errors:
-            return Response(errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(results, status=status.HTTP_201_CREATED)
-
     def get_queryset(self):
         return TwoVennProblemStatementModel.objects.filter(user=self.request.user)
 
+    def create(self, request, *args, **kwargs):
+        request.data['user'] = request.user.pk
+
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response({"message": "Saved Problem Statement"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     def update(self, request, *args, **kwargs):
 
         instance = self.get_object()
@@ -67,30 +53,17 @@ class ThreeVennProblemStatementView(mixins.ListModelMixin,
     queryset = ThreeVennProblemStatementModel.objects.all()
     serializer_class = ThreeProblemStatementSerializer
 
-    def create(self, request, *args, **kwargs):
-
-        statements = request.data.pop("statement")
-
-        results = []
-        errors = []
-
-        for statement in statements:
-            item_data = request.data.copy()
-            item_data['statement'] = statement
-            item_data['user'] = request.user.pk
-
-            serializer = self.get_serializer(data=item_data)
-            if serializer.is_valid():
-                serializer.save(user=request.user)
-                results.append(serializer.data)
-            else:
-                errors.append(serializer.errors)
-
-        if errors:
-            return Response(errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(results, status=status.HTTP_201_CREATED)
     def get_queryset(self):
         return ThreeVennProblemStatementModel.objects.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        request.data['user'] = request.user.pk
+
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response({"message": "Saved Problem Statement"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
