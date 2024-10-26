78dfcdf390372a07fb790ae7b95569cefed488e3
ErwinLambujon <erwinlambujon1@gmail.com>
2024-10-27 01:33:02 +0800

    Added Ranking Feature

--- api/Serializer/SubmissionVotingSerializer.py
+++ api/Serializer/SubmissionVotingSerializer.py
@@ -4,6 +4,13 @@ from api.Model.SubmissionVoting import SubmissionVotingMark
 
 
 class SubmissionVotingMarkSerializer(serializers.ModelSerializer):
+    voter_type = serializers.SerializerMethodField()
+
     class Meta:
         model = SubmissionVotingMark
         fields = '__all__'
+
+    def get_voter_type(self, obj):
+        # Since member_id is actually a ForeignKey to CustomUser,
+        # we can access the user_type directly
+        return obj.member_id.user_type  # Using member_id since that's your field name
